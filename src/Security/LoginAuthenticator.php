3d3efeeec0279f38162e1d4aefafcfa2358ec3bc
reddy <massidisemi@yahoo.fr>
2020-03-07 02:18:27 +0530

    change

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -99,7 +99,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($this->urlGenerator->generate('pharmacist'));
         }
 
-        //return new RedirectResponse($this->urlGenerator->generate('doctor'));
+        //return new RedirectResponse($this->urlGenerator->generate('doctor_new'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
