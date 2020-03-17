e4f2eaaa8643a40d850a3abd819595e78ef8839a
reddy <massidisemi@yahoo.fr>
2020-03-17 23:42:48 +0530

    contact working

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
+     * @var EntityManagerInterface
+     */
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+{
+    $this->manager = $manager;
+}
+
+    /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $contacts= new Contact();
+        $form=$this->createForm(ContactType::class ,$contacts);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->manager->persist($contacts);
+            $this->manager->flush();
+
+        }
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'form'=>$form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $fullName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $subject;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+
+    public function setFullName(string $fullName): self
+    {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+}
