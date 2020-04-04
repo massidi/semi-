59ecf0fcc4592c1bfef8b8025896cae6f9c7bf24
test91789797 <masiditresor@gmail.com>
2020-04-04 19:26:41 +0200

    form

--- src/Controller/Admin/PharmacistController.php
+++ src/Controller/Admin/PharmacistController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("Admin/pharmacist")
+ * @Route("Admin/")
  * Class PharmacistController
  * @package App\Controller\Admin
  */
